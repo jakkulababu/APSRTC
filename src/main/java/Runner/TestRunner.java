658a74a3b0fa5edabe81e740c23923dc0fd18b06
bjakkula <bjakkula@LIN80016387.corp.capgemini.com>
2019-02-15 20:58:41 +0530

    updated

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\bjakkula\\eclipse-workspace\\WaytoSMSBDDFrameWork\\src\\main\\java\\Features\\login.feature",
 		glue= {"stepDefinitions"},
